d306715446be0ec045591b86dc6bae9fad2511b9
Saptarshi Chatterjee <schatterjee@homeaway.com>
2017-11-12 21:04:19 -0600

    Chuck watch , use chokidar instead

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var watch = require('watch');
-var path  = require('path');
-var fs    = require('fs');
-
+var chokidar = require('chokidar');
 
 function watchFilePlugin(options) {
   this.options = options;
@@ -9,18 +6,50 @@ function watchFilePlugin(options) {
 
 watchFilePlugin.prototype.apply = function(compiler) {
   var that = this;
-  compiler.plugin("emit", function(compilation, callback) {
-    var watchFolder = that.options.watchFolder;
-    watch.createMonitor(watchFolder, function(monitor){
-      monitor.files[path.join(watchFolder, "/*."+that.options.watchExtension)]
-      monitor.on("changed", function (f, curr, prev) {
+  compiler.plugin('done', function(compilation) {
+
+    var watcher = chokidar.watch(that.options.watchFileRegex, {
+      persistent: that.options.persistance || true,
+      ignored: that.options.ignored || false,
+      ignoreInitial: that.options.ignoreInitial || false,
+      followSymlinks: that.options.followSymlinks || true,
+      cwd: that.options.cwd || '.',
+      disableGlobbing: that.options.disableGlobbing || false,
+      usePolling: that.options.usePolling || true,
+      interval: that.options.interval || 100,
+      binaryInterval: that.options.binaryInterval || 300,
+      alwaysStat: that.options.alwaysStat || false,
+      depth: that.options.depth || 99,
+      awaitWriteFinish: {
+        stabilityThreshold: that.options.stabilityThreshold || 2000,
+        pollInterval: that.options.pollInterval || 100
+      },
+
+      ignorePermissionErrors: that.options.ignorePermissionErrors || false,
+      atomic: that.options.atomic || true // or a custom 'atomicity delay', in milliseconds (default 100)
+    });
+
+    watcher
+      .on('add', path => console.log(`File ${path} has been added`))
+      .on('change', function(path) {
+        console.log("started  - "+ path);
         compiler.run(function(err) {
-          if(err) throw err;
+          if (err) throw err;
           monitor.stop();
         });
+        console.log("End - "+ path);
+      })
+      .on('unlink', path => console.log(`File ${path} has been removed`));
+
+    // More possible events.
+    watcher
+      .on('addDir', path => console.log(`Directory ${path} has been added`))
+      .on('unlinkDir', path => console.log(`Directory ${path} has been removed`))
+      .on('error', error => console.log(`Watcher error: ${error}`))
+      .on('ready', () => console.log('Initial scan complete. Ready for changes'))
+      .on('raw', (event, path, details) => {
+        console.log('Raw event info:', event, path, details);
       });
-    });
-    callback();
   });
 };
 
